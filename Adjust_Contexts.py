df6a01edc751e3100176110014d518546a269515
HigorItalo-GitHub <68316152+HigorItalo-GitHub@users.noreply.github.com>
2024-10-23 11:06:34 -0300

    Update Adjust_Contexts.py

--- Adjust_Contexts.py
+++ Adjust_Contexts.py
@@ -1,64 +1,60 @@
-def leafs_complementer(Folhas_Atual, Folhas, Alfabeto, probs_conds):
-    # cria um dicionario com elementos na forma {contexto: [(letra do alf.,contexto subsequente,prob. condicional do contexto associada a letra)]}
+"""The 'leafs_complementer' function implements the action of
+identifying suffixes and dividing sequences when necessary.
+It is associated with Step 2 of the PS2ER algorithm."""
 
-    print("\n-*-*- RUNNING STATE CREATION ROUTINE -*-*-")
+def leafs_complementer(Actual_Leafs, Leafs, Alphabet, probs_conds):
+    # Creates a dictionary with elements in the form
+    # {context: [
+    #           (letter of the alf., subsequent context, conditional prob. of the context associated with the letter)
+    #           ]
+    # }
+
+    # print("\n-*-*- RUNNING STATE CREATION ROUTINE -*-*-")
 
     new_ctx = []
 
-    for f in Folhas_Atual:  # para cada folha no conjunto de folhas atualmente analisado...
+    for f in Actual_Leafs:  # for each leaf in the currently analyzed leaf set...
 
         # print(F"\nANALYZING CONTEXT {f}:")
 
-        for letra in Alfabeto:  # ...tome cada uma das letras no alfabeto da sequencia...
+        for letra in Alphabet:  # ...take each of the symbols in the alphabet associated with sequence...
 
-            flag = True    # flag para detecção de ausencia de sufixo para uma concatenação
+            flag = True    # flag for detecting missing suffix for a concatenation
 
-            next = f + letra  # ... e faça a contanação da folha com a letra à esquerda, criando o contexto 'next'.
+            next = f + letra  # ... and contaminate the leaf with the symbol on the left, creating the 'next' context.
 
-            cont = 0  # variavel para realizar contabilização de não identificação de folhas como sufixo para 'next'
+            cont = 0  # variable to account for non-identification of leafs as a suffix for 'next'
 
-            for context in Folhas:  # para cada um dos contextos(folhas) no conjunto original de folhas analisado
+            for context in Leafs:  # For each of the contexts (leaves) in the original set of leaves analyzed...
 
-                if next.endswith(context):  # se alguma folha é identificada como sufixo da concatenação 'next'
-
-                    pass  # apenas indique a identificação
+                if next.endswith(context):  # ...if any leaf is identified as suffix of the 'next' concatenation...
 
                     # print(f"\nWORD {next} --> LEAF {context} IDENTIFIED AS A SUFFIX FOR {next}")
 
-                    flag = False
+                    flag = False    # flag to register the identification of a suffix for the analyzed concatenation
 
-                    # outs.append((letra, context, pbs[letra+"|"+f]))
-
-                # else:  # caso não se identifique qualquer folha como sufixo para 'next'...
-                #
-                #     # print(f" concatenation {next} not associated with leaf ",element)
-                #
-                #     cont += 1  # itere o contador
-
-            # if cont == len(Folhas):  # se o valor em 'cont' na rodada é igual ao total de folhas originais...
             if flag:
 
                 # print(
                 #     f"\nWORD {next} --> No leaf was identified among the current ones as a suffix for this context. "
                 #     f"Stored for addition to the current leafs set.")
 
-                new_ctx.append(f)  # armazene 'next' em "new_ctx' para ser adiconado às folhas originais
+                new_ctx.append(f)  # store 'next' in "new_ctx' to be added to the original leafs
 
         # print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 
-    if new_ctx:  # se houver algum contexto armazenado em 'new_ctx', realiza nova analise das folhas...
+    if new_ctx:  # If there is any context stored in 'new_ctx', perform a new analysis of the leaves...
 
         for ctx in new_ctx:
 
-            for simb in Alfabeto:
+            for simb in Alphabet:
                 add_ctx = simb + ctx
                 # print(f"ADDING CONTEXT {add_ctx} TO THE CURRENT SET OF SHEETS")
-                Folhas.append(add_ctx)
+                Leafs.append(add_ctx)
 
-            # if ctx in Folhas:
-            Folhas.remove(ctx)
+            Leafs.remove(ctx)
 
-        print("=================> REASSESSING LEAF SET AFTER INSECTIONS <=========================")
-        leafs_complementer(Folhas, Folhas, Alfabeto, probs_conds)  # ...e reexecutando esta função.
+        # print("=================> REASSESSING LEAF SET AFTER INSECTIONS <=========================")
+        leafs_complementer(Leafs, Leafs, Alphabet, probs_conds)  # ...e reexecutando esta função.
 
-    return Folhas  # RETORNA AS FOLHAS DEVIDAMENTE COMPLEMENTADAS
+    return Leafs  # RETURN THE LEAVES SET PROPERLY COMPLETED
