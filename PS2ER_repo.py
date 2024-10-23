6395444bffed0825a61c1579ba7f73be0dd0217d
HigorItalo-GitHub <68316152+HigorItalo-GitHub@users.noreply.github.com>
2024-10-23 11:06:05 -0300

    Update PS2ER_repo.py

--- PS2ER_repo.py
+++ PS2ER_repo.py
@@ -1,16 +1,17 @@
 import Sequences_Analyser
 import numpy as np
 import CT_Functions
-import Sequence_Generator
 import Adjust_Contexts
-from Eigenvector import occup_vector
 
 
-def calc_H(alf, leafs_set, probs, probs_conds):
+"""The 'calc_H' function implements the calculation of the 
+Entropy function H, based on the probability data associated with the 
+sequences corresponding to the sequencies in 'leafs_set'"""
+def calc_H(alphabet, leafs_set, probs, probs_conds):
     H = 0
 
     for q in leafs_set:
-        for sigma in alf:
+        for sigma in alphabet:
             if f'{sigma}|{q}' in probs_conds[len(q)] and not probs_conds[len(q)][f'{sigma}|{q}'] == 0:
                 # print("ok")
                 H -= probs[len(q) - 1][q] * probs_conds[len(q)][sigma + '|' + q] * np.log2(
@@ -18,59 +19,10 @@ def calc_H(alf, leafs_set, probs, probs_conds):
 
     return H
 
-
-def split_H(Q_set, ALFABETO, PROBS, COND_PROBS):
-    # print("\n*** SPLIT VIA REDUÇÃO DE ENTROPIA ***")
-    # print("Conjunto de folhas de entrada:\n", Q_set)
-
-    ent_concs = {}
-    break_H, registred_h = None, None
-
-    for estado in Q_set:
-
-        actual_leafs = Q_set.copy()
-
-        for e in actual_leafs:
-
-            if e == estado:
-
-                for symb in ALFABETO:
-                    actual_leafs.append(symb + estado)
-
-                actual_leafs.remove(e)
-
-        # print("Conjunto de possives folhas a analisar:\n", actual_leafs)
-
-        # P_est = PROBS[len(estado) - 1][estado]
-        # print(f"P({estado}) = {P_est}")
-
-        entropy_H = calc_H(ALFABETO, actual_leafs, PROBS, COND_PROBS)
-        # print(f"H = {entropy_H}")
-        ent_concs[entropy_H] = actual_leafs
-
-        # print("dicionario criado:", ent_concs)
-    # print("Conjuntos possiveis de folhas a analisar:")
-    # for set in ent_concs.values():
-    #     print(set)
-
-    for h in ent_concs.keys():
-        if h == min(ent_concs.keys()):
-            # print(f"Conjunto de folhas que gera menor H ({h}):\n", ent_concs[h])
-            registred_h = h
-            break_H = ent_concs[h]
-
-    # print("dicionario criado:", ent_concs)
-
-    # print("menor entropia H calculada: ", registred_h)
-
-    # print("\n=> Conjunto de folhas reajustado:\n", break_H)
-    # return break_H
-    return break_H, registred_h
-
-
-def split_H_depthed(Q_set, depth, ALFABETO, PROBS, COND_PROBS):
-    # print("\n*** SPLIT VIA REDUÇÃO DE ENTROPIA ***")
-    # print("Conjunto de folhas de entrada:\n", Q_set)
+"""The 'split_H_depthed' function is responsible for 
+governing the extension of nodes, until max lenght 'depth', in the context tree, 
+referring to Step 1 of the PS2ER algorithm."""
+def split_H_depthed(Q_set, depth, ALPHABET, PROBS, COND_PROBS):
 
     ent_concs = {}
     break_H, registred_h = None, None
@@ -83,62 +35,21 @@ def split_H_depthed(Q_set, depth, ALFABETO, PROBS, COND_PROBS):
 
             if e == estado and len(e) < depth:
 
-                for symb in ALFABETO:
+                for symb in ALPHABET:
                     actual_leafs.append(symb + estado)
 
                 actual_leafs.remove(e)
 
-        # print("Conjunto de possives folhas a analisar:\n", actual_leafs)
+        entropy_H = calc_H(ALPHABET, actual_leafs, PROBS, COND_PROBS)
 
-        # P_est = PROBS[len(estado) - 1][estado]
-        # print(f"P({estado}) = {P_est}")
-
-        entropy_H = calc_H(ALFABETO, actual_leafs, PROBS, COND_PROBS)
-        # print(f"Split do estado {estado}: H = {entropy_H}")
-        # print(f"H = {entropy_H}")
-        # if entropy_H in ent_concs:
-        #     print(f"==> Mais de uma sequencia apresenta H = {entropy_H}")
         ent_concs[entropy_H] = actual_leafs
-
-        # print("{Entropia : sequencias consideradas} -> ", ent_concs)
-
-    # print("Conjuntos possiveis de folhas a analisar:")
-    # for set_h, set in sorted(ent_concs.items()):
-    #     print(f"{set_h}: {set}")
-    # input("continue")
 
     for h in ent_concs.keys():
         if h == min(ent_concs.keys()):
-            # print(f"Conjunto de folhas que gera menor H ({h}):\n", ent_concs[h])
             registred_h = h
             break_H = ent_concs[h]
 
-    # print("dicionario criado:", ent_concs)
-
-    # print("menor entropia H calculada: ", registred_h)
-
-    # print("\n=> Conjunto de folhas gerados por split_H:\n", break_H)
-
     return break_H, registred_h
-
-
-def dist_euclidiana(vector):
-    import itertools
-    import math
-
-    x = list(itertools.combinations(vector, 2))
-    dists = []
-    for h in x:
-        v1, v2 = np.array(h[0]), np.array(h[1])
-        # print(v1, v2)
-        diff = v1 - v2
-        # print(diff)
-        quad_dist = np.dot(diff, diff)
-        # print(quad_dist)
-        dists.append(round((math.sqrt(quad_dist)), 4))
-        # print(f'    Distancia euclidiana entre {v1} e {v2}: %.3f' % math.sqrt(quad_dist))
-
-    return dists[0]
 
 
 if __name__ == "__main__":
@@ -152,6 +63,8 @@ if __name__ == "__main__":
             sequence = line
     sequence_lenght = len(sequence)
 
+    print(f"\nOriginal symbolic sequence:\n{sequence[0:100]}...")
+
     """Call to modules that return probabilities of 
     symbolic sequences obtained from the original MaFaulDa symbolic series."""
     L = 35  # Limit variable for calculating symbolic sequences of length L
@@ -161,164 +74,83 @@ if __name__ == "__main__":
     Sigma = sorted(seq_alf)  # orders the alphabet of symbols associated with the analyzed sequence
 
     """Defining the maximum number of leafs (N_max) to be generated for the leafs set (Gamma)"""
-    Gamma = ['']
-    N_leafs = [18, 25, 42, 78, 122, 142, 178, 202, 228, 250]
+    Gamma = ['']    # Initialization of the Gamma leaf set with the empty word (root of the context tree)
+    N_leafs = [18, 25, 42, 78, 122, 142, 178, 202, 228, 250]    # 'N_max' values used do generate multiple PS2ER models.
     l_max = 9  # tree depth threshold - corresponds to the maximum length allowed for leaves
 
     save_maq = False  # Variable that enables storing the generated PFSA in a pickle file (.pkl)
 
+    param = 10  # Parameter adopted do quantifiers performance calculation
+    original_seq_entropy = None # variable for storing the conditional entropy reference value calculated for the analyzed system
     States = []     # stores the labels of the states of the PFSA to be generated
-    Ents = []   # stores conditional entropies calculated for a PFSA obtained for the number of leaves defined in the variable 'Folhas'
+    Ents = []       # stores conditional entropies calculated for a PFSA obtained for the number of leaves defined in the variable 'Folhas'
     DivergsKL = []  # stores Kullback-Leibler divergences calculated for a PFSA obtained for the number of leaves defined in the variable 'Folhas'
 
+    model_name = "PS2ER"    # label used do name some archive created
+
     for N_max in N_leafs:
-        while len(Gamma) < N_max:  #
+        while len(Gamma) < N_max:   # Loop executed until the maximum number of leaves determined, 'N_max'
+
+            """The call to the 'split_H_depthed' function at this point performs 
+            the extension of nodes of the generated context-tree until a max depth 'l_max', 
+            corresponding to the action performed in Step 1 of the PS2ER algorithm."""
             Gamma, h = split_H_depthed(Gamma, l_max, Sigma, probabilities, conditional_probabilities)
-            model_name = "PS2ER"
 
-            Gamma = Adjust_Contexts.leafs_complementer(Gamma, Gamma, Sigma, conditional_probabilities);
-            model_name = "PS2ER"
+            """The call to the 'leafs_complementer' function of 'Adjust_Context' auxiliar module at this point performs 
+            the suffix identification action for each of the leaves in the current Gamma set, 
+            corresponding to the action performed in Step 2 of the PS2ER algorithm"""
+            Gamma = Adjust_Contexts.leafs_complementer(Gamma, Gamma, Sigma, conditional_probabilities)
 
-        used_states_vI, actual_machine_vI, n_states_vI, generated_sequence = \
+        """Once the set of adjusted leaves has been defined (with the completion of the previous loop), 
+        the set of leaves is used at this point to construct the states in the PFSA model, 
+        corresponding to that performed in Step 3 of the PS2ER algorithm."""
+        numbr_of_model_states, generated_PFSA, n_states, generated_sequence = \
             CT_Functions.new_sequence_generator_v2(
                 Gamma, Sigma, probabilities, conditional_probabilities, sequence_lenght)
-        # expanded_Q_Splittings, alfabeto_da_sequencia, probabilidades, probabilidades_condicionais, sequence_lenght)
 
         new_sequence = generated_sequence
-        # new_sequence = Sequence_Generator.generate_sequence(actual_machine_vI, sequence_lenght)
 
-        # with open(f'MACHINE_MaFaulDa_Splitting_{len(Q_splitings)}.pkl', 'wb') as output:
-        #     pickle.dump(actual_machine_vI, output, pickle.HIGHEST_PROTOCOL)
-
-        # from Comparing_Morphs import calc_morphs_dists
-        # registers = {}
-        # lim = 0.0605
-        # for state in actual_machine_vI.states:
-        #     # print(state.name, state.outedges)
-        #     for morph in state.outedges:
-        #         if state.name + morph[0] == morph[1]:
-        #             pass
-        #         else:
-        #             d = calc_morphs_dists([state.name + morph[0],morph[1]],alfabeto_da_sequencia,probabilidades_condicionais)
-        #             if d >= lim:
-        #                 if state.name not in registers:
-        #                     registers[state.name] = [f"{morph} --> dist[{state.name + morph[0]},{morph[1]}] = {d}"]
-        #                 else:
-        #                     registers[state.name].append(f"{morph} --> dist[{state.name + morph[0]},{morph[1]}] = {d}")
-        #
-        # for k,v in registers.items():
-        #     print(k,v)
-        # print(f"\n===Quantidade de estados que apresentam morphs com distancia entre transições acima do limiar {lim}: {len(registers)}===")
-
-        # input("continue")
-
-        machine = actual_machine_vI
-        counting_ins = {state.name: 0 for state in machine.states}
-        for state in machine.states:
-            for outedge in state.outedges:
-                counting_ins[outedge[1]] += 1
-        print("\n==> Nº de in-edges para cada um dos estados do PFSA gerado:")
-        for k, v in counting_ins.items():
-            print(f"{k}: {v}")
-
-        """=============================================================================================================="""
-        trans_mtx = np.zeros((len(actual_machine_vI.states), len(actual_machine_vI.states)))
-        # print(trans_mtx)
-        num_mach_sts = {st: num for num, st in enumerate(actual_machine_vI.states)}
-        # for a,b in num_mach_sts.items():
-        #     print(a.name,b)
-
-        for stt, idx in num_mach_sts.items():
-            i_row = idx
-            i_col = None
-            for oe in stt.outedges:
-                # print(oe)
-                stt_col = oe[1]
-                for s_k, s_v in num_mach_sts.items():
-                    if s_k.name == stt_col:
-                        i_col = s_v
-                # print("dest: ", i_col)
-                # print("---")
-                #     print(i_col)
-                trans_mtx[i_row][i_col] = 1
-
-        # for line in trans_mtx:
-        #     print(line)
-        # input("cont.")
-
-        # trans_mtx = np.array([[0,0,1,1],[1,1,0,0],[0,0,1,1],[0,0,1,0]]) # teste
-
-        verif_mtx = np.linalg.matrix_power(trans_mtx, len(actual_machine_vI.states))
-
-        # for lin in verif_mtx:
-        #     print(lin)
-        # input("cont.")
-
-        print("\nIndices de elementos iguais a 0 no array 'verif_mtx':")
-        # res = np.where(verif_mtx == 0)[0]
-        res = np.argwhere(verif_mtx == 0)
-        print(res)
-        # input("cont.")
-
-        if save_maq:
+        if save_maq:    # allow save the PFSA model in a .pkl archive, if 'save_maq' is setted as 'True'
             import pickle
+            with open(f'{system_name}_MACHINE_{model_name}_N{n_states}.pkl', 'wb') as output_maq:
+                pickle.dump(generated_PFSA, output_maq, pickle.HIGHEST_PROTOCOL)
 
-            with open(f'{system_name}_MACHINE_{model_name}_N{n_states_vI}.pkl', 'wb') as output_maq:
-                pickle.dump(actual_machine_vI, output_maq, pickle.HIGHEST_PROTOCOL)
+        """vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv QUANTIFIERS PERFORMANCE CALCULATION vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"""
 
-        print("Vetor de ocupação téorico da máquina gerada:")
-        for occ in occup_vector(actual_machine_vI):
-            print(round(occ, 6))
-        # input("CONT-->")
+        print(f"\n---PERFOMRMING CONDITIONAL ENTROPY CALCULATION FROM GENERATED SEQUENCE FOR WORDS OF LENGHT L={param}---")
+        generated_sequence_entropy = CT_Functions.entropy_analyser(new_sequence, param)
 
-        """=============================================================================================================="""
+        print(f"\n---PERFOMRMING KULLBACK-LEIBLER DIVERGENCE CALCULATION FROM GENERATED SEQUENCE FOR WORDS OF LENGHT L={param}---")
+        generated_sequence_probabilities, alf_seq_new_sequence = Sequences_Analyser.calc_probs(new_sequence, param)
+        generated_sequence_divergKL = Sequences_Analyser.calc_kldivergence(generated_sequence_probabilities, probabilities, param)
 
-        param = 10
+        print(f"\n---PERFOMRMING CONDITIONAL ENTROPY CALCULATION FROM ORIGINAL SEQUENCE FOR WORDS OF LENGHT L={param}---")
+        original_seq_entropy = CT_Functions.entropy_analyser(sequence, param)
 
-        entropia_new_sequence = CT_Functions.entropy_analyser(new_sequence, param)
+        # print(f"Coditional Entropy h{param} of system:\n", original_seq_entropy[-1])
 
-        probabilidades_new_sequence, alf_seq_new_sequence = Sequences_Analyser.calc_probs(new_sequence, param)
+        # print("\nTotal of states in generated PFSA:\n", len(used_states_vI))
+        # print(f"Entropy h{param} of generated model:\n", generated_sequence_entropy[-1])
 
-        divergKL_new_sequence = Sequences_Analyser.calc_kldivergence(probabilidades_new_sequence, probabilities, param)
+        # print(f"Kullback-Leibler Divergence D{param} of generated model:\n{generated_sequence_divergKL}")
 
-        print("========================== RESULTADOS ===========================\n")
-        entropia_original_seq = CT_Functions.entropy_analyser(sequence, param)
-        print(f"Entropia h{param} sistema original:\n", entropia_original_seq[-1])
+        print("\n========================== MODEL ANALYSES CONCLUDED! ===========================\n")
 
-        print("\nTotal de estados do PFSA gerado:\n", len(used_states_vI))
-        print(f"Entropia h{param} do modelo gerado:\n", entropia_new_sequence[-1])
-
-        print(f"Divergencia Kullback-Leibler D{param} do modelo gerado:\n{divergKL_new_sequence}")
-
-        States.append(len(used_states_vI))
-        Ents.append(entropia_new_sequence[-1])
-        DivergsKL.append(divergKL_new_sequence)
+        """ storing generated model data """
+        States.append(len(numbr_of_model_states))          # store number os states of the generated PFSA
+        Ents.append(generated_sequence_entropy[-1]) # store conditional entropy for given 'param' of the generated PFSA
+        DivergsKL.append(generated_sequence_divergKL)   # store Kullback-Leibler divergence for given 'param' of the generated PFSA
 
         Gamma = ['']
 
-    print(f"Entropia h{param} sistema original:\n", entropia_original_seq[-1])
+    """The results presented below can be used to 
+    plot graphs of the quantifiers adopted versus the number of states of the PS2ER model generated."""
 
-    print("\nEstados dos PFSAs gerados:\n", States)
-    print(f"Entropias h{param} dos modelos gerados:\n", Ents)
-    print(f"Divergencias Kullback-Leibler D{param} dos modelos gerados:\n{DivergsKL}")
+    print("\n==========================================================\n"
+          "========================= RESULTS ========================\n"
+          "==========================================================\n")
 
-    # num_folhas = []
-    # regists_H = []
-    #
-    # reg_H = 1.0
-    # folhas = ['']
-    # num_f = 23
-    #
-    # while reg_H > 0.1231:
-    # # while len(folhas) <= num_f:
-    #
-    #     folhas, reg_H = split_H(folhas, alfabeto_da_sequencia, probabilidades, probabilidades_condicionais)
-    #     # if len(folhas) == num_f:
-    #     print(len(folhas), folhas)
-    #     num_folhas.append(len(folhas))
-    #     regists_H.append(reg_H)
-    #
-    #     # folhas = split_compair(folhas, alfabeto_da_sequencia, probabilidades_condicionais, 'euclid')
-    #
-    # print(num_folhas)
-    # print(regists_H)
+    print(f"Entropy h{param} from original system (baseline):\n", original_seq_entropy[-1])
+    print("Number of states:\n", States)
+    print(f"Entropy h{param}:\n", Ents)
+    print(f"Kullback-Leibler D{param}:\n{DivergsKL}")
